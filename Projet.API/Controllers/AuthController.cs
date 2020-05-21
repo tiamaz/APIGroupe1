fb7f54e7bee4aedc2105589d5081bcea3745eb31
younes tiamaz <tiamaz@MBP-de-younes.Home>
2020-05-21 14:08:20 +0000

    Seance 3

--- Projet.API/Controllers/AuthController.cs
+++ Projet.API/Controllers/AuthController.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Projet.API.Dto;
 using Projet.API.Model;
 
@@ -20,12 +26,18 @@ namespace Projet.API.Controllers
         private readonly RoleManager<Role> _roleManager;
         private readonly SignInManager<User> _signInManager;
 
-        public AuthController(UserManager<User> userManager, IMapper mapper, RoleManager<Role> roleManager,SignInManager<User> signInManager )
+        private readonly IConfiguration _config;
+
+        public AuthController(UserManager<User> userManager, 
+        IMapper mapper, RoleManager<Role> roleManager,
+        SignInManager<User> signInManager,
+        IConfiguration config )
         {
             _userManager = userManager;
             _mapper = mapper;
             _roleManager = roleManager;
             _signInManager = signInManager;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -50,37 +62,86 @@ namespace Projet.API.Controllers
           var result =   await _userManager.CreateAsync(user, userForRegisterDto.password);
           if(result.Succeeded)
           {
-              await _userManager.AddToRoleAsync(user, roles[0].Name);
+              if(user.Status =="prof")
+              {
+                await _userManager.AddToRoleAsync(user, roles[0].Name); 
+              }
+              else{
+                  await _userManager.AddToRoleAsync(user, roles[1].Name); 
+              }
+              
               return Ok("L'enregistrement réussi");
           }
           return BadRequest(result.Errors);
 
         }
         
-         public async Task<IActionResult> Login(UserForLoginDto userForLogin)
+    [HttpPost("login")]
+       public async Task<IActionResult> Login(UserForLoginDto userForLogin)
         {
             var user = await _userManager.FindByNameAsync(userForLogin.UserName);
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, userForLogin.password,false);
 
+
             if(result.Succeeded)
             {
+                var roles = await _userManager.GetRolesAsync(user);
                     //// Créer un JWT : Json Web Token
-                return Ok("Ok");
+                return Ok(
+                    new {
+                        token = GenerateJwtToken(user, roles)
+                    }
+                    );
             }
 
             return BadRequest("username ou password sont erronés");
 
         }
 
-       /*  public async Task<string> GenerateJwtToken(User user, IList<string> roles){
+        [Authorize]
+        [HttpGet("salim")]
+        public IActionResult hello(){
+            return Ok("Bonjour 4IIR1");
+        }
+ 
+        
+          private async Task<string> GenerateJwtToken(User user, IList<string> roles){
 
+                //// Définir les informations à transmettre
                 var claims = new List<Claim>
                 {
-                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName)     
                 };
 
-        } */
+                foreach(var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                //// Choisir le code et le crypter
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+                    ////Décrir le Token
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = creds,
+                Expires = DateTime.Now.AddDays(1)
+            };
+
+                    /// Ajouter un porteur de mon token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                    ////Ecrire mon Token
+            return tokenHandler.WriteToken(token);
+
+
+        } 
+
+
     }
 
    
--- Projet.API/Startup.cs
+++ Projet.API/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Projet.API.Data;
 using Projet.API.Model;
 
@@ -36,17 +39,39 @@ namespace Projet.API
                 )
             );
 
-            IdentityBuilder builder = services.AddIdentityCore<User>();
+            IdentityBuilder builder = services.AddIdentityCore<User>(
+
+                opt =>{
+                opt.Password.RequireDigit = false;
+                opt.Password.RequiredLength = 4;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireLowercase = false;}
+            );
             builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
             builder.AddEntityFrameworkStores<ApplicationDbContext>();
             builder.AddRoleValidator<RoleValidator<Role>>();
             builder.AddRoleManager<RoleManager<Role>>();
             builder.AddSignInManager<SignInManager<User>>();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                        .AddJwtBearer(Options => {
+                        Options.TokenValidationParameters = new TokenValidationParameters{
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                                Configuration.GetSection("AppSettings:Token").Value)),
+                                ValidateIssuer = false,
+                                ValidateAudience = false
+                                };
+                        });
+
+
             services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -59,6 +84,8 @@ namespace Projet.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
